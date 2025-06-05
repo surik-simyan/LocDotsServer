81ec6c34d13300b7f53493f6218626c64df696d5
scrooge <surik.simyan@gmail.com>
2025-06-05 16:12:44 +0400

    Project Setup

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.plugin.serialization)
+}
+
+group = "surik.simyan.locdots"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.mongodb.driver.core)
+    implementation(libs.mongodb.driver.sync)
+    implementation(libs.bson)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+}
